83b8cd97d6eed7ac16fe9a6de2ccd271eaf14152
Michael Walker <walkerrepair255@yahoo.com>
2021-11-18 17:56:05 -0500

    component connections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ['h1 {color: blue;}']
 })
-export class AppComponent {
-  title = 'student-list';
+export class AppComponent implements OnInit {
+  title = 'Noodles';
+  name: string = "";
+  display: boolean = false;
+  subject: any = null;
+
+  colors: any[]= [
+    "red","white", "blue", "yellow", "green"
+  ]
+
+  clicked(): void {
+    this.title = "Fred's student list";
+  }
+  unclicked(): void {
+    this.title = "Noodles";
+  }
+
+  ngOnInit(): void {
+    console.debug("This is a debug message")
+  }
 }
+
